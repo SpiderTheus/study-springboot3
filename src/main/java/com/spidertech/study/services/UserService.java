1cb015da49cc462be6308ea77de956b99815a909
SpiderTheus <matemora80@gmail.com>
2025-05-29 11:50:57 -0300

    Service Layer, Component registration

---
+++ src/main/java/com/spidertech/study/services/UserService.java
@@ -0,0 +1,27 @@
+package com.spidertech.study.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spidertech.study.entities.User;
+import com.spidertech.study.repositories.UserRepository;
+
+@Service
+public class UserService {
+		
+	@Autowired
+	private UserRepository repository;
+	
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
